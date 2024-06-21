2bbb1f2a6bc9422021ebf969b522eb0cf58ffa00
lab-testt <saydhany012@gmail.com>
2024-06-22 01:22:57 +0300

    first commit

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
-CREATE TABLE persons (
+-- init.sql
+CREATE TABLE IF NOT EXISTS persons (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     age INT
@@ -8,3 +9,4 @@ INSERT INTO persons (name, age) VALUES
 ('John Doe', 30),
 ('Jane Smith', 25),
 ('Alice Johnson', 22);
+
